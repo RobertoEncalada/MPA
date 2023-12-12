82d423e3b1fd54fbeba5578d07a764e0544e4c32
roberto encalada <rocaenca@espol.edu.ec>
2023-12-11 20:31:42 -0500

    Nuevo Repositorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component'
+import { FooterComponent } from './shared/footer/footer.component'
+import { IndexComponent } from './pages/index/index.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { Netflix } from './interfaces/netflix';
+import { NetflixService } from './providers/netflix.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,FooterComponent,IndexComponent,HttpClientModule],
+  providers: [NetflixService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MPA';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioItemComponent } from './pages/portfolio-item/portfolio-item.component';
+import { PortfolioOverviewComponent } from './pages/portfolio-overview/portfolio-overview.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+export const routes: Routes = [
+    { path: 'index', component: IndexComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'portfolio-item', component: PortfolioItemComponent },
+    { path: 'portfolio-overview', component: PortfolioOverviewComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'pricing', component: PricingComponent}
+];
