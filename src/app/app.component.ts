564384159733c0904b63c03c39a5e11e590ce5f7
katumbac <katumbac@espol.edu.ec>
2023-12-13 13:40:22 -0500

    update index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component'
-import { FooterComponent } from './shared/footer/footer.component'
 import { IndexComponent } from './pages/index/index.component';
+import { FooterComponent } from './shared/footer/footer.component'
 import { HttpClientModule } from  '@angular/common/http';
 import { NetflixService } from './providers/netflix.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavbarComponent,IndexComponent,FooterComponent,HttpClientModule],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,FooterComponent,IndexComponent,HttpClientModule],
   providers: [NetflixService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
