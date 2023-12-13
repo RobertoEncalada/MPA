f85185e9404cae31f2fae78be463f891c30e4dcb
katumbac <katumbac@espol.edu.ec>
2023-12-13 01:52:21 -0500

    update

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -14,7 +14,7 @@ import { NetflixService } from '../../providers/netflix.service';
 export class AboutComponent {
   public data : Netflix[] = [];
   constructor(private dataProvider: NetflixService) { }
-  title = 'mpa';
+  titleAbout = 'Acerca de Netflix';
 
   ngOnInit() {
     this.dataProvider.getResponse().subscribe((response) => { 
@@ -22,4 +22,8 @@ export class AboutComponent {
       this.data = dataArray.slice(0,10);
     })
   }
+
+  strTitle:string = "Creado por Usuarios, Para Usuarios: La Historia de Netflix"
+  strInfo:string = 'Descubre la historia detrás de la plataforma que ha transformado la forma en que consumimos entretenimiento. Desde su fundación por Reed Hastings y Marc Randolph hasta su expansión global, exploraremos cómo Netflix ha mantenido su compromiso de ofrecer contenido diverso y
+   de calidad.'
 }
