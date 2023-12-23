618f83f91cd2d51783c2effb7a835025ecde6437
roberto encalada <rocaenca@espol.edu.ec>
2023-12-23 16:19:18 -0500

    filtrado de datos en la tabla y agregación de datos en las páginas

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 import { Title } from '@angular/platform-browser';
 import { HttpClientModule } from  '@angular/common/http';
 import { Netflix } from '../../interfaces/netflix';
 import { NetflixService } from '../../providers/netflix.service';
+
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule,HttpClientModule],
+  imports: [CommonModule,HttpClientModule,FormsModule],
   providers: [NetflixService],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
   public data : Netflix[] = [];
-
   public filteredData: Netflix[] = [];
   public filterType: string = 'all'; // In
 
@@ -23,7 +24,7 @@ export class AboutComponent {
   ngOnInit() {
     this.dataProvider.getResponse().subscribe((response) => { 
       let dataArray = (response as Netflix[]); 
-      this.data = dataArray.slice(0,10);
+      this.data = dataArray.slice(0,25);
       this.filteredData = this.data;
 
       this.titleService.setTitle('Acerca de');
