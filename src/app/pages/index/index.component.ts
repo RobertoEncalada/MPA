9496e1e57b107dfb9381975667d8ba3ac6653edb
roberto encalada <rocaenca@espol.edu.ec>
2023-12-26 22:16:47 -0500

    Ruteo de los botones de Home

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLinkActive, RouterLink } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 @Component({
