3fe72d2831b3cdc8427a35cc347128f74d5bd84b
roberto encalada <rocaenca@espol.edu.ec>
2023-12-26 22:24:41 -0500

    Ruteo de los botones de Home otra vez

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -6,7 +6,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLinkActive, RouterLink],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
